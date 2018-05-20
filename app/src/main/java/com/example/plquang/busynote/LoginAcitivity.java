e5545a6875036fc907d98b1a8edc2a12becc5066
PLQuang <phanlequang99@gmail.com>
2018-05-20 12:45:10 +0700

    #1 Style login

---
+++ app/src/main/java/com/example/plquang/busynote/LoginAcitivity.java
@@ -0,0 +1,21 @@
+package com.example.plquang.busynote;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginAcitivity extends AppCompatActivity {
+Button btnlog;
+Button btnreg;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acitivity);
+
+
+    }
+
+}
