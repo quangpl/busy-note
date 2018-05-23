42c6f670eafd696c7c06b687090f6afbc9699b41
PLQuang <phanlequang99@gmail.com>
2018-05-23 12:09:12 +0700

    #1 Add animation at WelcomeActivity

--- app/src/main/java/com/example/plquang/busynote/Welcome.java
+++ app/src/main/java/com/example/plquang/busynote/Welcome.java
@@ -5,19 +5,24 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class Welcome extends AppCompatActivity {
-ImageView btn;
+ImageView button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        btn=(ImageView) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        final Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+       button=(ImageView) findViewById(R.id.btn);
+
+       button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                button.startAnimation(animation);
                 Intent intent=new Intent(Welcome.this,MainActivity.class);
                 startActivity(intent);
             }
