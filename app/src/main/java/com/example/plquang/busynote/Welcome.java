9adb6eebd1646adb44f44d2d206dd233b9f4890b
PLQuang <phanlequang99@gmail.com>
2018-05-23 23:21:44 +0700

    #1 Update animation

--- app/src/main/java/com/example/plquang/busynote/Welcome.java
+++ app/src/main/java/com/example/plquang/busynote/Welcome.java
@@ -2,8 +2,11 @@ package com.example.plquang.busynote;
 
 import android.content.Intent;
 import android.media.Image;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -11,12 +14,26 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class Welcome extends AppCompatActivity {
-ImageView button;
+Animation animation;
+View layout;
+private static  int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        final Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+
+
+                                          Intent homeIntent = new Intent(Welcome.this, MainActivity.class);
+                                          startActivity(homeIntent);
+                                          overridePendingTransition(R.anim.out,R.anim.trans);
+                                          finish();
+                                      }
+                                  },SPLASH_TIME_OUT);
+        /*final Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
        button=(ImageView) findViewById(R.id.btn);
 
        button.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +43,6 @@ ImageView button;
                 Intent intent=new Intent(Welcome.this,MainActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 }
